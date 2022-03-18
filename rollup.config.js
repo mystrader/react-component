d40c11189a4af85d5e2e7dafceb65f9036e39834
Marcus Cunha <mystrader@gmail.com>
2022-03-18 14:29:55 -0300

    o restante

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+
 const packageJson = require("./package.json");
 
 export default [
@@ -21,14 +23,22 @@ export default [
             },
         ],
         plugins: [
+             // NEW
+      peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            // NEW
+            postcss(),
+            terser(),
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+
+        // NEW
+        external: [/\.css$/],
     },
 ];
